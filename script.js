484c637c4bd8f4dfb474921415dbc6e87156a755
Roland <roland.onofrej@akademiasovy.sk>
2017-10-25 12:33:27 +0200

    added checks for inputs

--- script.js
+++ script.js
@@ -2,18 +2,33 @@ $(document).ready(function() {
    $("#btnOk").click(function(event) {
          var fname=$("#fname").val();
          if(fname.trim().length<2){
-           console.log("menej ako 2");
            $("#errfname").html("Invalid first name");
-
          }
+
+         var lname=$("#lname").val();
+         if(lname.trim().length<2){
+           console.log("menej ako 2");
+           $("#errlname").html("Invalid last name");
+         }
+         var bday=new Date($("#birthday").val());
+         //var day=bday.getMonth();
+         console.log(bday);
    });
 
    $("#fname").keyup(function(event) {
-     console.log("sss");
      var fname=$("#fname").val();
      if(fname.trim().length>1){
        $("#errfname").html("");
      }
      /* Act on the event */
    });
+
+   $("#lname").keyup(function(event) {
+     var lname=$("#lname").val();
+     if(lname.trim().length>1){
+       $("#errlname").html("");
+     }
+     /* Act on the event */
+   });
+
 });
